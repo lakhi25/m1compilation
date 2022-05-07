cd40ba75d08d9e3e68397e92a8eaed67d55ea43b
lakshana yathavarayar <yathavarayarlakshana@gmail.fr>
2022-05-07 21:11:41 +0200

    Erreur not required here

---
+++ AST.ml
@@ -0,0 +1,25 @@
+
+type expression_a =
+| Plus  of expression_a * expression_a
+| Moins of expression_a * expression_a
+| Mult  of expression_a * expression_a
+| Modulo of expression_a * expression_a
+| Neg   of expression_a
+| Num   of int
+;;
+
+
+(* Fonctions d'affichage *)
+
+let rec print_binaire form s g d = Format.fprintf form "@[<2>%s%s@ %a%s@ %a%s@]" s "(" print_AST g " ," print_AST d " )" 
+
+and print_AST form = let open Format in function
+| Plus  (g,d) -> print_binaire form "Plus" g d
+| Moins (g,d) -> print_binaire form "Moins" g d
+| Mult  (g,d) -> print_binaire form "Mult" g d
+| Modulo  (g,d) -> print_binaire form "Modulo" g d
+| Neg    e    -> fprintf form "@[<2>%s@ %a@]" "Neg" print_AST e 
+| Num    n    -> fprintf form "@[<2>%s@ %i@]" "Num" n
+;; 
+
+
